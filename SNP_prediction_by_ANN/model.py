9c0eafa5fe46b2e0feb4bf01203ffc374319a67b
MuhammadZainButt0 <121681402+MuhammadZainButt0@users.noreply.github.com>
2025-07-16 23:37:52 +0500

    simple feedforward model

---
+++ SNP_prediction_by_ANN/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class DNAClassifier(nn.Module):
+    def __init__(self, input_dim, hidden_dim=64, output_dim=2):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x 
